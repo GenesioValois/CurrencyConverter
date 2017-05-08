487ac18b3f4ebeff6565a615f8349d499a734694
ubuntu <genesiovalois@gmail.com>
2017-05-08 18:40:38 -0300

    refatoring feature test

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -8,8 +8,7 @@ RSpec.describe 'Exchange Currency Process', type: :feature do
       select('USD', from: 'currency_destination')
       fill_in 'quantity', with: '10'
     end
-    # click_button 'CONVERTER'
-    page.execute_script("$('#exchange_form').submit()")
+    click_button 'CONVERTER'
     # save_and_open_page
     expect(page).to have_content('value')
   end
